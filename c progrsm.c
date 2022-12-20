18f50fe8afeedcda8c5591d8f0f605da79911e2e
tejashri1402 <121081983+tejashri1402@users.noreply.github.com>
2022-12-20 12:08:51 +0530

    Update c program.c

--- c progrsm.c
+++ c progrsm.c
@@ -3,6 +3,14 @@ main()
 {
   printf("\n Welcome");
   printf("\n TO MY PROGRAM ");
-  
-  
+  add();
+}
+
+void add()
+{
+  int a,b,c;
+  printf("\n Enter a and b: ");
+  scanf("%d%d"&a,&b);
+  c=a+b;
+  printf("\n Addition is :%d",c)
 }
